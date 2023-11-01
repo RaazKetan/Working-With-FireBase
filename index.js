97aedff04d6d8222a1fea029f4c0ace0db4040dd
Raaz <21ketanraaz@gmail.com>
2023-11-01 21:12:34 +0530

    Fetching from databse

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
 
-import {getDatabase, ref , push} from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js"
+import {getDatabase, ref , push, onValue} from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-3df1d-default-rtdb.firebaseio.com/"
@@ -14,9 +14,34 @@ const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 const shoppingListEl =document.getElementById("shopping-list");
 
+
 addButtonEl.addEventListener("click",()=>{
     let inputValue = inputFieldEl.value;
     push(shoppingListInDB, inputValue);
-    inputFieldEl.value  = "";
-    shoppingListEl.innerHTML+= `<li> ${inputValue}</li>`
+    clearInputFieldEl();
 });
+
+// call the onValue function with shoppingListInDB as the first argument and fucntion(snapshot){} as the second argument
+onValue(shoppingListInDB,function(snapshot){
+
+    //use object.value to convert snapshot.val() from an Object to and Array. Create a variable for this
+    let itemsArray = Object.values(snapshot.val())
+
+    //BUg where the databse fetch all item when new item is added
+
+    shoppingListEl.innerHTML = ""
+
+    // loop to ierate on itemsArray and console.log each item
+    for(let i = 0;i<itemsArray.length;i++){
+        // use he appendItemToShoppingListEl(itemValue) function inside of the for loop to append item to the shopping list element for each iteration
+    appendItemToShoppingListEl(itemsArray[i]);
+    }
+})
+
+
+function clearInputFieldEl(){
+    inputFieldEl.value = ""
+}
+function appendItemToShoppingListEl(itemValue){
+    shoppingListEl.innerHTML+= `<li> ${itemValue}</li>`
+}
